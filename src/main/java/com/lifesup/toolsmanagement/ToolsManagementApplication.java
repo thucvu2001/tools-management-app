5b41317da56cde3f9a7bc65dc25f69d36e2bb468
Thuc Vu <vuthuc3152001@gmail.com>
2023-08-10 17:16:23 +0700

    10/08/2023

---
+++ src/main/java/com/lifesup/toolsmanagement/ToolsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.lifesup.toolsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolsManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lifesup/toolsmanagement/ToolsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifesup.toolsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
